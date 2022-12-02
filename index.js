3aea21a7cb46ecc17247abdd6feaf96b4ebb6249
VikasMeshram2708 <meshramvikas744@gmail.com>
2022-12-02 21:18:40 +0530

    done

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const port = process.env.PORT;
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
+const yup = require("yup");
+const idgen = require("idgen");
+const monk = require("monk");
+const db = monk(process.env.MONGO_URI);
+const urls = db.get("urls");
+// urls.createIndex("name");
+urls.createIndex("slug");
+// const { nanoid } = require("nanoid");
+// nanoid();
+
+// Schema
+const schema = yup.object().shape({
+  slug: yup
+    .string()
+    .trim()
+    // .matches(/[a-z0-9_-]/i),
+    .matches(/[\w\-]/i),
+  url: yup.string().trim().url().required(),
+});
 
 // middlewares
 app.use(cors());
@@ -21,17 +40,64 @@ app.post("/url/:id", (req, res) => {
   });
 });
 
-app.get("/:id", (req, res) => {
+app.get("/:id", async (req, res, next) => {
   // TODO : redirect to url
-  res.json({
-    message: "bhagg 🏹",
-  });
+  const { id: slug } = req.params;
+  try {
+    const url = await urls.findOne({ slug });
+    if (url) {
+      res.redirect(url.url);
+    }
+    res.redirect(`/?error=${slug} not found`);
+  } catch (error) {
+    res.redirect("/?error=Link not found");
+  }
 });
 
-app.post("/url", (req, res) => {
+app.post("/url", async (req, res, next) => {
   // TODO : create a short url
+  let { slug, url } = req.body;
+  try {
+    await schema.validate({
+      slug,
+      url,
+    });
+    if (!slug) {
+      slug = idgen(5);
+    } else {
+      // make sure it's no existing
+      const existing = await urls.findOne({ slug });
+      if (existing) {
+        throw new Error("Slug in use ... 🍔");
+      }
+    }
+    slug = slug.toLowerCase();
+    console.log(slug);
+    // const secret = idgen(10).toLocaleLowerCase();
+    const newUrl = {
+      url,
+      slug,
+    };
+    const created = await urls.insert(newUrl);
+    res.json(created);
+  } catch (error) {
+    // if (error.message.startsWith("E11000")) {
+    //   error.message = "Slug in use ... 🍔";
+    // }
+    next(error);
+  }
+});
+
+// error handler
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
   res.json({
-    message: "bhagg 🏹",
+    message: error.message,
+    stack: process.env.NODE_ENV === "production" ? "🍰" : error.stack,
   });
 });
 
--- public/index.js
+++ public/index.js
@@ -1,1 +1,30 @@
-console.log("hello,world");
+const form = document.querySelector("form");
+const generated = document.querySelector(".generated");
+
+const api_URI = "http://localhost:5000/url";
+
+async function formSubmitted(event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const url = formData.get("url");
+  const slug = formData.get("slug");
+  const data = {
+    url,
+    slug,
+  };
+  //   console.log(data);
+  const response = await fetch(api_URI, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const json = await response.json();
+  console.log(json);
+  //   generated.innerHTML = <a href={`https://localhost:5000/${slug}`}></a>;
+  generated.innerHTML = `http://localhost:5000/${slug}`;
+
+  //   `https://localhost:5000/${json.slug}}`;
+}
+form.addEventListener("submit", formSubmitted);
